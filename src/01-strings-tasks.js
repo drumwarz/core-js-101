e732accf035216500d9b59d5b1070cac03f30775
Ihar Sapeha <maddavoice@gmail.com>
2022-12-02 01:01:25 +0300

    feat: add 02-numbers-task solution

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -200,7 +200,7 @@ function extractEmails(str) {
  */
 function getRectangleString(width, height) {
   return `┌${'─'.repeat(width - 2)}┐\n${`│${' '.repeat(width - 2)}│\n`.repeat(
-    height - 2
+    height - 2,
   )}└${'─'.repeat(width - 2)}┘\n`;
 }
 
